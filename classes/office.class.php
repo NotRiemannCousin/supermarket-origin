7c013f1ac6667d6a212f5c9229e551d9a7867590
MarceloH1122 <marcelohenrique8822@gmail.com>
2022-11-29 16:42:42 -0300

    offices initial config

---
+++ classes/office.class.php
@@ -0,0 +1,44 @@
+<?php
+include_once 'permission.enum.php.php';
+include_once 'user.class.php';
+
+
+class Administrator extends User
+{
+    protected $permissions = [
+        Permission::SeeProducts,
+        Permission::AddProducts,
+        Permission::DeleteProducts,
+        Permission::SeeOthers,
+        Permission::EditOthers
+    ];
+}
+class CommonUser extends User
+{
+    protected $permissions = [
+        Permission::SeeProducts
+    ];
+}
+class Cashier extends User
+{
+    protected $permissions = [
+        Permission::SeeProducts,
+        Permission::AddProducts,
+        Permission::DeleteProducts
+    ];
+}
+class HRManager extends User
+{
+    protected $permissions = [
+        Permission::SeeProducts,
+        Permission::SeeOthers,
+        Permission::EditOthers
+    ];
+}
+class Counter extends User
+{
+    protected $permissions = [
+        Permission::SeeProducts,
+        Permission::SeeOthers
+    ];
+}
